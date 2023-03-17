8df7b6527caf10a8e5b102371fa7d07d3caec1a3
alfandi <fandidixpc1@gmail.com>
2023-03-17 11:19:00 +0700

    Membuat  array bubblesort

--- BubbleShort/BubbleShort.cpp
+++ BubbleShort/BubbleShort.cpp
@@ -8,13 +8,21 @@ int main()
     std::cout << "Hello World!\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+#include <iostream
+using namespace std;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int a[20]; //deklarasi array dengan ukuran 20
+int n;     // deklarasi variabel n untuk menyimpan banyaknya elemen pada array
+
+void input() {  // procedure untuk input
+    while (true) {  // looping
+        cout, , "Masukkan banyaknya elemen pada array: "; // output ke layar
+        cin >> n;    //input dari pengguna
+        if (n <= 20)   // jika n kurang dari atau sama dengan 20
+            break;    // keluar dari loop
+        else {    //jika n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; // output ke layar
+
+        }
+    }
+}
